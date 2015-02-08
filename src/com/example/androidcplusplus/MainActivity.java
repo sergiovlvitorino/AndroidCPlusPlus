8c8fa016750dac3967dfde92e1303f2441d4b145
sergio <sergiovlvitorino@gmail.com>
2015-02-08 06:00:58 -0200

    improving layout

--- src/com/example/androidcplusplus/MainActivity.java
+++ src/com/example/androidcplusplus/MainActivity.java
@@ -2,20 +2,52 @@ package com.example.androidcplusplus;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	private EditText edResult;
+	private EditText edValue1;
+	private EditText edValue2;
+	private Button btSum;
+	private Calculator calculator;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);	
-		runOnUiThread(new Runnable() {
+		initializeComponents();
+		configureButtons();
+	}
+
+	private void configureButtons() {
+		btSum.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void run() {
-				Toast.makeText(MainActivity.this, ""+new Calculator().sum(1, 2), Toast.LENGTH_LONG).show();
+			public void onClick(View v) {
+				final float value1 = parseValue(edValue1.getText().toString());
+				final float value2 = parseValue(edValue2.getText().toString());
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						edResult.setText(Float.toString(calculator.sum(value1, value2)));
+					}
+				});
 			}
 		});
+	}
+
+	private void initializeComponents() {
+		edResult = (EditText) findViewById(R.id.edResult);
+		edValue1 = (EditText) findViewById(R.id.edValue1);
+		edValue2 = (EditText) findViewById(R.id.edValue2);
+		btSum = (Button) findViewById(R.id.btSum);
+		calculator = new Calculator();
+	}
+	
+	public float parseValue(String value){
+		return Float.parseFloat(value == null || "".equals(value) ? "0" : value);
 	}
 	
 }
